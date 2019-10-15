d43c73d4f27b215733aef78171879a46f7eb9f7b
PRASHANTHTALACAUVERY <PRASHANTHTALACAUVERY@DESKTOP-66HIDK2>
2019-10-15 14:52:51 +0530

    Initial commit.

---
+++ deliverysubscription/src/main/java/com/albertsons/ecommerce/deliverysubscription/DeliverysubscriptionApplication.java
@@ -0,0 +1,13 @@
+package com.albertsons.ecommerce.deliverysubscription;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeliverysubscriptionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverysubscriptionApplication.class, args);
+	}
+
+}
---
+++ deliverysubscription/src/test/java/com/albertsons/ecommerce/deliverysubscription/DeliverysubscriptionApplicationTests.java
@@ -0,0 +1,16 @@
+package com.albertsons.ecommerce.deliverysubscription;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class DeliverysubscriptionApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
